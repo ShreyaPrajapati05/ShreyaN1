e57e4673664bbc90655941645d4c7cb85339a14a
ShreyaPrajapati05 <61162159+ShreyaPrajapati05@users.noreply.github.com>
2020-03-04 23:13:56 -0800

    Update index.js
    SSL False

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: false
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
